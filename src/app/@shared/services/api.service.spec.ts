0d2c21c31258a8ddb5e0d4693409fd0ad711e81a
Nathan Pinciak <n.pinciak@criteo.com>
2021-02-13 18:54:37 -0500

    Create base api service

---
+++ src/app/@shared/services/api.service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ApiService } from './api.service';
+
+describe('ApiService', () => {
+  let service: ApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[]
+    });
+    service = TestBed.inject(ApiService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/@shared/services/api.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) { }
+
+  /**
+   * GET request
+   * @param endpoint Request endpoint. Ex: /insights/user
+   * @param options Request headers, query params, etc.
+   * @param mock Substitute mock data for real http
+   */
+  public get<T>(endpoint: string, options?: object): Observable<T> {
+    return this.http.get<T>(endpoint, options);
+  }
+
+  /**
+   * POST request
+   * @param endpoint Request endpoint. Ex: /insights/user
+   * @param data Request payload
+   * @param options Request headers, query params, etc.
+   */
+  public post<T>(endpoint: string, postData: object, options?: object): Observable<T> {
+    return this.http.post<T>(endpoint, postData, options);
+  }
+
+  /**
+   * PUT request
+   * @param endpoint Request endpoint. Ex: /insights/user
+   * @param data Request payload
+   * @param options Request headers, query params, etc.
+   */
+  public put<T>(endpoint: string, putData: object, options?: object): Observable<T> {
+    return this.http.put<T>(endpoint, putData, options);
+  }
+}
