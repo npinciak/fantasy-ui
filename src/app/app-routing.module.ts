1f38c248ce216175f330b17f04940865bb3cd788
Nathan Pinciak <n.pinciak@criteo.com>
2021-02-19 13:01:46 -0500

    Add router outlet to base template, add basic ESPN routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from "@angular/core";
-import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EspnComponent } from './modules/espn/espn.component';
 
-const routes: Routes = []
+const routes: Routes = [
+    { path: 'espn', component: EspnComponent, }
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
     providers: []
 })
