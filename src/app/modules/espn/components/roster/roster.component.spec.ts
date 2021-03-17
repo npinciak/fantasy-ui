0c3a044ab8c9f4b98f4e9d893166114ad8cdb03e
npinciak <natepinciak@live.com>
2021-03-17 12:12:30 -0400

    Update roster component to support player rankings

--- src/app/modules/espn/components/roster/roster.component.spec.ts
+++ src/app/modules/espn/components/roster/roster.component.spec.ts
@@ -1,25 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from 'src/app/material.module';
+import { mockMLBTeam } from '../../models/mlb-team.mock';
 
 import { RosterComponent } from './roster.component';
 
 describe('RosterComponent', () => {
   let component: RosterComponent;
   let fixture: ComponentFixture<RosterComponent>;
+  let compiled;
+
+  const getByTestId = (testId: string) => compiled.querySelector(`[data-test-id="${testId}"]`);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RosterComponent ]
+      imports: [RouterTestingModule, MaterialModule, BrowserAnimationsModule],
+      declarations: [RosterComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RosterComponent);
     component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/modules/espn/components/roster/roster.component.ts
+++ src/app/modules/espn/components/roster/roster.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { EspnService, Sports } from '../../espn.service';
 import { FantasyPlayer } from '../../models/fantasy-player.class';
 
 @Component({
@@ -7,21 +9,39 @@ import { FantasyPlayer } from '../../models/fantasy-player.class';
   templateUrl: './roster.component.html',
   styleUrls: ['./roster.component.scss']
 })
-export class RosterComponent implements OnInit {
+export class RosterComponent implements OnChanges {
   @Input() fantasyPlayers: FantasyPlayer[];
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
   dataSource = new MatTableDataSource<FantasyPlayer>();
 
   readonly rosterColumns = [
     'lineupSlot',
     'name',
-    'ownership'
+    'positionalRankingSeason',
+    'ownershipChange',
+    'percentOwned'
   ];
 
-  constructor() { }
+  constructor(private espnService: EspnService) { }
 
-  ngOnInit(): void {
-    this.dataSource.data = this.fantasyPlayers;
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName in changes) {
+      if (changes.hasOwnProperty(propName)) {
+        switch (propName) {
+          case 'fantasyPlayers':
+            this.fantasyPlayers = changes[propName].currentValue;
+            this.dataSource.data = this.fantasyPlayers;
+            this.dataSource.sort = this.sort;
+            // console.log(this.fantasyPlayers)
+            break;
+          default:
+            break;
+        }
+      }
+    }
   }
+
+  playerInfo = (id: number) => this.espnService.getPlayer(7, id, Sports.mlb).subscribe(res => console.log(res.feed[0]));
 
 }
