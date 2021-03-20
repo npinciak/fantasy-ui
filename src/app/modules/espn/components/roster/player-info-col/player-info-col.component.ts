beea6f74c7ba89ab7e50c38820ae84d6d1fb2650
npinciak <natepinciak@live.com>
2021-03-20 19:05:26 -0400

    Create playerInfo component for table column

---
+++ src/app/modules/espn/components/roster/player-info-col/player-info-col.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-player-info-col',
+  templateUrl: './player-info-col.component.html'
+})
+export class PlayerInfoColComponent {
+  @Input() player: any;
+  constructor() { }
+
+
+}
