d42875eb0138c77cd9827c2f5cf9a897105041e8
npinciak <natepinciak@live.com>
2021-05-31 12:43:26 -0400

    Update espn.component for MLB only

--- src/app/modules/espn/espn.component.ts
+++ src/app/modules/espn/espn.component.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-debugger */
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngxs/store';
@@ -14,19 +13,12 @@ export class EspnComponent implements OnInit {
   readonly sport = this.activatedRoute.snapshot.params.sport;
   readonly leagueId = this.activatedRoute.snapshot.params.leagueId;
 
-  constructor(
-    readonly mlbFacade: MlbFacade,
-    private activatedRoute: ActivatedRoute,
-    private store: Store) { }
+  constructor(readonly mlbFacade: MlbFacade, private activatedRoute: ActivatedRoute, private store: Store) { }
 
   ngOnInit(): void {
-        this.getMLBLeague(this.leagueId);
-    }
+    this.getMLBLeague(this.leagueId);
   }
-  private getMLBLeague = (leagueId: number) => this.store.dispatch(new FetchBaseballLeague(leagueId));
 
-  private getNFLLeague = (leagueId: number) => this.espnFacade.getLeague(leagueId);
-  private getMLBLeague = (leagueId: number) => this.espnFacade.getLeague(leagueId);
-  private getFreeAgents = (leagueId: number) => this.espnFacade.getFreeAgents(leagueId);
+  private getMLBLeague = (leagueId: number) => this.store.dispatch(new FetchBaseballLeague(leagueId));
 
 }
