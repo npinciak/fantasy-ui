7649273afd5d4d64d591919bbf60a2f18d7c9634
npinciak <natepinciak@live.com>
2021-06-13 12:33:11 -0400

    Update test case for espn.service

--- src/app/modules/espn/espn.service.spec.ts
+++ src/app/modules/espn/espn.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { NgxsModule, Store } from '@ngxs/store';
 
 import { EspnService } from './espn.service';
 
@@ -9,11 +10,13 @@ describe('EspnService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [EspnService]
+      imports: [HttpClientTestingModule, NgxsModule.forRoot()],
+      providers: [EspnService, Store]
     });
+
     service = TestBed.inject(EspnService);
     httpTestingController = TestBed.inject(HttpTestingController);
+
   });
 
   afterEach(() => {
@@ -23,5 +26,17 @@ describe('EspnService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call fetchEspnBaseball', () => {
+
+    const spy = spyOn(service, 'fetchEspnBaseball').and.callThrough();
+
+    service.fetchEspnBaseball(1);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+
+
 
 });
