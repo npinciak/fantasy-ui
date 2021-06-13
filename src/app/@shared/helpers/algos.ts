679735c3f3876ac4cfd43f5e22d905fc8117da17
npinciak <natepinciak@live.com>
2021-06-13 17:13:26 -0400

    Add some sort and search algos for practice

---
+++ src/app/@shared/helpers/algos.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/prefer-for-of */
+
+/**
+ * Insertion sort
+ *
+ * @param arr []
+ * @param sortProperty
+ * @returns
+ */
+const insertionSortDesc = (arr, sortProperty) => {
+    if (!Array.isArray(arr)) {
+        return [];
+    }
+
+    const length = arr.length;
+    for (let i = 1; i < length; i++) {
+        const key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j][sortProperty] < key[sortProperty]) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+};
+
+
+const binarySearch = (arr, val) => {
+    let low = 0;
+    let high = arr.length - 1;
+    while (low <= high) {
+        // find mid range
+        const mid = (low + high) / arr.length;
+        const guess = arr[mid];
+        if (guess === val) {
+            return mid;
+        }
+
+        if (guess > val) {
+            // update high to mid -1
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return null;
+};
+
+
+export { insertionSortDesc, binarySearch };
