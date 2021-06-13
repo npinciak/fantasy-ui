da3426e0901fac3b01c59b8314bf7506ebd24301
npinciak <natepinciak@live.com>
2021-06-13 12:29:15 -0400

    Combine league and event methods for concurrent requests

--- src/app/modules/espn/espn.service.ts
+++ src/app/modules/espn/espn.service.ts
@@ -1,5 +1,7 @@
 import { HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { forkJoin, of } from 'rxjs';
+import { currentDate } from 'src/app/@shared/helpers/date';
 
 import { ApiService } from 'src/app/@shared/services/api.service';
 import { League, Player } from './models/mlb/interface';
@@ -15,21 +17,30 @@ export enum Sports {
 
 export class EspnService {
 
-  private fantasyBase = 'https://fantasy.espn.com/apis/v3';
-  private apiBase = 'https://site.api.espn.com/apis';
+  private readonly fantasyBase = 'https://fantasy.espn.com/apis/v3';
+  private readonly apiBase = 'https://site.api.espn.com/apis';
 
   constructor(private api: ApiService) { }
 
-  getBaseballLeague = (leagueId: number) =>
+  fetchEspnBaseball = (leagueId: number) => {
+    const $fantasyLeague = this._baseballLeague(leagueId);
+    const $games = this._baseballEvents();
+    return forkJoin([$fantasyLeague, $games]);
+  };
+
+
   updateTeam = (payload: unknown, leagueId: number) =>
     this.api.post<any>(`${this.fantasyBase}/games/flb/seasons/2021/segments/0/leagues/${leagueId}/transactions`, payload, {
       withCredentials: true, headers: this.postHeaders
     });
+
+
+  private readonly _baseballLeague = (leagueId: number) =>
     this.api.get<League>(`${this.fantasyBase}/games/${Sports.baseball}/seasons/${this.currentYear}/segments/0/leagues/${leagueId}`,
       { params: this.params }
     );
 
-  getBaseballPlayerNews = (days: number, playerId: number) =>
+  private readonly _getBaseballPlayerNews = (days: number, playerId: number) =>
     this.api.get<any>(`${this.apiBase}/fantasy/v2/games/${Sports.baseball}/news/players`, {
       params: new HttpParams().set('days', days.toString()).set('playerId', playerId.toString())
     });
@@ -47,6 +58,27 @@ export class EspnService {
       }
     );
 
+  /**
+   * Retrieve games for current date
+   *
+   * @description Fetches espn fantasy api for current games for today
+   * @returns list of events
+   */
+  private readonly _baseballEvents = () =>
+    this.api.get<EventList>(`${this.apiBase}/fantasy/v2/games/${Sports.baseball}/games`, {
+
+      params: this.baseballEventParams
+    });
+  /**
+   * @todo
+   */
+  private get baseballEventParams() {
+    let params = new HttpParams();
+    params = params.append('useMap', 'true');
+    params = params.append('dates', currentDate());
+    // params = params.append('pbpOnly', 'true');
+    return params;
+  }
   private get faParams() {
     let params = new HttpParams();
     params = params.append('view', 'kona_player_info');
