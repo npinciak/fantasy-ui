5b281fd19be7b47d113509437e4d8e53319ee70e
npinciak <natepinciak@live.com>
2021-06-15 13:28:38 -0400

    Add snackbar for http error handling

--- src/app/@core/interceptors/error-handler.interceptor.ts
+++ src/app/@core/interceptors/error-handler.interceptor.ts
@@ -8,11 +8,12 @@ import {
 } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class ErrorHandlerInterceptor implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(catchError((error: HttpErrorResponse) => this.errorHandler(error)));
@@ -21,8 +22,13 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
   private errorHandler(response: HttpErrorResponse): Observable<HttpEvent<any>> {
     if (response.error instanceof ErrorEvent) {
     } else {
+
       const msg = response.message || 'NO_MSG';
       const code = response.status || 0;
+      const statusText = response.statusText || 'NO_STATUS';
+
+      this.snackBar.open(`${code}: ${statusText}`, 'x', { panelClass: ['mat-toolbar', 'mat-warn'] });
+
       return of(response as any);
     }
   }
