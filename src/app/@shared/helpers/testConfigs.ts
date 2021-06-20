839185181a7d91008b8458da3bb04c1517b59559
npinciak <natepinciak@live.com>
2021-06-20 12:31:28 -0400

    Add updated mock data to testConfigs

--- src/app/@shared/helpers/testConfigs.ts
+++ src/app/@shared/helpers/testConfigs.ts
@@ -1,32 +1,29 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import { MockGame, MockLeague, MockTeam } from '@app/modules/espn/models/mlb/mocks';
 import { BaseballTeam } from '@espn/models/mlb/class/team.class';
-import * as mockleague from '@espn/models/mlb/mocks/league.mock.json';
-import { teamMap } from './mapping';
+import { currentDate } from './date';
+import { newTeamMap } from './mapping';
 
 const TEST_ID = {
     STAT_TOGGLE: {
         BATTING_ROTO: 'battingRoto',
         PITCHING_ROTO: 'pitchingRoto'
     }
-
 };
 
 const MOCK_DATA = {
-    BASEBALL_TEAM: new BaseballTeam(mockleague.teams[0]),
-    BASEBALL_TEAM_MAP: teamMap(mockleague.teams, mockleague.schedule)
+    BASEBALL_TEAM: new BaseballTeam(MockTeam),
+    BASEBALL_TEAM_MAP: newTeamMap(MockLeague.teams, MockLeague.schedule),
+    LEAGUE_ID: 8675309,
+    // eslint-disable-next-line max-len
+    LEAGUE_REQUEST: `https://fantasy.espn.com/apis/v3/games/flb/seasons/2021/segments/0/leagues/8675309?view=mLiveScoring&view=mMatchupScore&view=mRoster&view=mScoreboard&view=mTeam`,
+    ESPN_GAME_REQUEST: `https://site.api.espn.com/apis/fantasy/v2/games/flb/games?useMap=true&dates=${currentDate()}`,
+    ESPN_TEAM: MockTeam,
+    ESPN_LEAGUE: MockLeague,
+    ESPN_EVENT: MockGame.events[0],
+    ESPN_SCHEDULE: MockLeague.schedule
 };
 
-
-
 const getByTestId = (testId: string) => document.querySelectorAll(`[data-test-id*=${testId}]`);
-
-
-// export const testAttr = (attr: string) => {
-//     return `[data-test="${attr}"]`;
-//   };
-//
-//   export const getByTestAttr = (attr: string, options = {}, childSelector = '') =>
-//     cy.get(`${testAttr(attr)} ${childSelector}`.trim(), options);
-
 
 export { getByTestId, MOCK_DATA, TEST_ID };
