ec772d83aa5fddd932a53df4fb0069c9037a769f
npinciak <natepinciak@live.com>
2021-06-20 12:05:46 -0400

    Add new class for current weather conditions handling

--- src/app/modules/weather/weather/models/class/currentConditions.class.ts
+++ src/app/modules/weather/weather/models/class/currentConditions.class.ts
@@ -1,7 +1,32 @@
-class CurrentConditions {
-    constructor() {
+import { precipitationMap, weatherMap } from '../maps';
 
+class CurrentConditions {
+    private _dataFields: any;
+
+    constructor(dataFields: any) {
+        this._dataFields = dataFields;
     }
+
+    get weather() {
+        return weatherMap[this._dataFields.weatherCode];
+    }
+
+    get temperature() {
+        return this._dataFields.temperature;
+    }
+
+    get realFeel() {
+        return this._dataFields.temperatureApparent;
+    }
+
+    get precipitation() {
+        return precipitationMap[this._dataFields.precipitationType];
+    }
+
+    get chanceOf() {
+        return `Chance of ${precipitationMap[this._dataFields.precipitationType]}, ${this._dataFields.precipitationProbability}%`;
+    }
+
 }
 
 export { CurrentConditions };
