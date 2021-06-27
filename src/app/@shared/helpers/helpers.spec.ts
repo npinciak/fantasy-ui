4ef94217da5efae43d121c4ce32ed52a81db2127
npinciak <natepinciak@live.com>
2021-06-27 12:06:39 -0400

    Create tests for gameMap method

--- src/app/@shared/helpers/helpers.spec.ts
+++ src/app/@shared/helpers/helpers.spec.ts
@@ -1,99 +1,116 @@
 import { gameMap, newTeamMap, rosterMap, teamMap } from './mapping';
 import * as mockleague from '@espn/models/mlb/mocks/league.mock.json';
+import { MOCK_DATA } from './testConfigs';
 
-describe('TeamMap', () => {
+describe('[Helpers]', () => {
 
-    it('should return empty map if team size is 0', () => {
-        const actual = teamMap([], mockleague.schedule);
-        const expected = 0;
+    describe('TeamMap', () => {
 
-        expect(actual.size).toEqual(expected);
+        it('should return empty map if team size is 0', () => {
+            const actual = teamMap([], mockleague.schedule);
+            const expected = 0;
+
+            expect(actual.size).toEqual(expected);
+        });
+
+        it('should return empty map if schedule size is 0', () => {
+            const actual = teamMap(mockleague.teams, []);
+            const expected = 0;
+
+            expect(actual.size).toEqual(expected);
+        });
+
+        it('should return map of teams', () => {
+            const actual = teamMap(mockleague.teams, mockleague.schedule);
+            const expected = 1;
+
+            expect(actual.size).toEqual(expected);
+        });
+
+        it('should return team with liveScore of 0 if no matching schedule', () => {
+            const noMatchingTeam = [{
+                teams: [{
+                    teamId: 55,
+                    totalPoints: 78,
+                    totalPointsLive: 45
+                }]
+            }];
+
+            const actual = teamMap(mockleague.teams, noMatchingTeam).get(6).liveScore;
+            const expected = 0;
+
+            expect(actual).toEqual(expected);
+        });
     });
 
-    it('should return empty map if schedule size is 0', () => {
-        const actual = teamMap(mockleague.teams, []);
-        const expected = 0;
+    describe('RosterMap', () => {
 
-        expect(actual.size).toEqual(expected);
+        it('should return empty map if roster size is 0', () => {
+            const actual = rosterMap([]);
+            const expected = 0;
+
+            expect(actual.size).toEqual(expected);
+        });
+
+        it('should return map of teams', () => {
+            const roster = teamMap(mockleague.teams, mockleague.schedule).get(6).roster;
+            const actual = rosterMap(roster);
+            const expected = 2;
+
+            expect(actual.size).toEqual(expected);
+        });
     });
 
-    it('should return map of teams', () => {
-        const actual = teamMap(mockleague.teams, mockleague.schedule);
-        const expected = 1;
+    describe('newTeamMap', () => {
 
-        expect(actual.size).toEqual(expected);
+        it('should return empty map if team size is 0', () => {
+            const actual = newTeamMap({}, mockleague.schedule);
+            const expected = 0;
+            expect(Object.values(actual).length).toEqual(expected);
+        });
+
+        it('should return empty map if schedule size is 0', () => {
+            const actual = newTeamMap(mockleague.teams, []);
+            const expected = 0;
+
+            expect(Object.values(actual).length).toEqual(expected);
+        });
+
+        it('should return map of teams', () => {
+            const actual = newTeamMap(mockleague.teams, mockleague.schedule);
+            const expected = 1;
+
+            expect(Object.values(actual).length).toEqual(expected);
+        });
+
+        it('should return team with liveScore of 0 if no matching schedule', () => {
+            const noMatchingTeam = [{
+                teams: [{
+                    teamId: 55,
+                    totalPoints: 78,
+                    totalPointsLive: 45
+                }]
+            }];
+
+            const actual = newTeamMap(mockleague.teams, noMatchingTeam)[6].liveScore;
+            const expected = 0;
+
+            expect(actual).toEqual(expected);
+        });
     });
 
-    it('should return team with liveScore of 0 if no matching schedule', () => {
-        const noMatchingTeam = [{
-            teams: [{
-                teamId: 55,
-                totalPoints: 78,
-                totalPointsLive: 45
-            }]
-        }];
+    describe('gameMap', () => {
+        it('should return empty map if game size is 0', () => {
+            const actual = gameMap({});
+            const expected = 0;
+            expect(Object.values(actual).length).toEqual(expected);
+        });
 
-        const actual = teamMap(mockleague.teams, noMatchingTeam).get(6).liveScore;
-        const expected = 0;
+        it('should return map of games', () => {
+            const actual = gameMap({ [Number(MOCK_DATA.ESPN_EVENT.id)]: MOCK_DATA.ESPN_EVENT });
+            const expected = 1;
 
-        expect(actual).toEqual(expected);
-    });
-});
-
-describe('RosterMap', () => {
-
-    it('should return empty map if roster size is 0', () => {
-        const actual = rosterMap([]);
-        const expected = 0;
-
-        expect(actual.size).toEqual(expected);
-    });
-
-    it('should return map of teams', () => {
-        const roster = teamMap(mockleague.teams, mockleague.schedule).get(6).roster;
-        const actual = rosterMap(roster);
-        const expected = 2;
-
-        expect(actual.size).toEqual(expected);
-    });
-});
-
-
-
-describe('newTeamMap', () => {
-
-    it('should return empty map if team size is 0', () => {
-        const actual = newTeamMap({}, mockleague.schedule);
-        const expected = 0;
-        expect(Object.values(actual).length).toEqual(expected);
-    });
-
-    it('should return empty map if schedule size is 0', () => {
-        const actual = newTeamMap(mockleague.teams, []);
-        const expected = 0;
-
-        expect(Object.values(actual).length).toEqual(expected);
-    });
-
-    it('should return map of teams', () => {
-        const actual = newTeamMap(mockleague.teams, mockleague.schedule);
-        const expected = 1;
-
-        expect(Object.values(actual).length).toEqual(expected);
-    });
-
-    it('should return team with liveScore of 0 if no matching schedule', () => {
-        const noMatchingTeam = [{
-            teams: [{
-                teamId: 55,
-                totalPoints: 78,
-                totalPointsLive: 45
-            }]
-        }];
-
-        const actual = newTeamMap(mockleague.teams, noMatchingTeam)[6].liveScore;
-        const expected = 0;
-
-        expect(actual).toEqual(expected);
+            expect(Object.values(actual).length).toEqual(expected);
+        });
     });
 });
