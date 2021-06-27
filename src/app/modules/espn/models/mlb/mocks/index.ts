75fd6e3d38f8012db769ac6746e0cb5ded625cc1
npinciak <natepinciak@live.com>
2021-06-27 12:07:25 -0400

    Create mock transaction response

--- src/app/modules/espn/models/mlb/mocks/index.ts
+++ src/app/modules/espn/models/mlb/mocks/index.ts
@@ -2,6 +2,6 @@ import * as MockLeague from './league.mock.json';
 import * as MockPlayer from './player.mock.json';
 import * as MockTeam from './team.mock.json';
 import * as MockGame from './game.mock.json';
+import * as MockTransaction from './transaction.mock.json';
 
-
-export { MockPlayer, MockLeague, MockTeam, MockGame };
+export { MockPlayer, MockLeague, MockTeam, MockGame, MockTransaction };
