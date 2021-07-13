6be73140809a9cab8cfab9c4746a08fece470e7c
npinciak <natepinciak@live.com>
2021-07-13 09:47:31 -0400

    Create new league scoreboard to differentiate between actual scoreboard

---
+++ src/app/modules/espn/components/league-scoreboard/league-scoreboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-league-scoreboard',
+  templateUrl: './league-scoreboard.component.html',
+  styleUrls: ['./league-scoreboard.component.css']
+})
+export class LeagueScoreboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
