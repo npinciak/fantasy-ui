70f202b2d1f08651a31810b5ef88b4de5b60fb87
npinciak <natepinciak@live.com>
2021-07-13 10:44:55 -0400

    Create no-data component

---
+++ src/app/@shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NoDataComponent } from './components/no-data/no-data.component';
+
+@NgModule({
+  declarations: [
+    NoDataComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
