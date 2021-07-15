7c45dd760d6d71a22e64c04ef2cf7ed92f07e3e3
npinciak <natepinciak@live.com>
2021-07-15 17:46:47 -0400

    Create page layout container

---
+++ src/app/@shared/components/page-layout/page-layout.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { GRID_TEMPLATE } from '@app/@shared/helpers/grid';
+
+@Component({
+  selector: 'app-page-layout',
+  templateUrl: './page-layout.component.html',
+  styleUrls: ['./page-layout.component.scss'],
+})
+export class PageLayoutComponent implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  readonly GRID_TEMPLATE = GRID_TEMPLATE;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
