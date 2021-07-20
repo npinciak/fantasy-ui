9d0795028be2ec05f175372cf4b0b1879ebac2de
npinciak <natepinciak@live.com>
2021-07-20 14:04:24 -0400

    Add pageTitle input to page layout

--- src/app/@shared/components/page-layout/page-layout.component.ts
+++ src/app/@shared/components/page-layout/page-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GRID_TEMPLATE } from '@app/@shared/helpers/grid';
 
 @Component({
@@ -7,6 +7,8 @@ import { GRID_TEMPLATE } from '@app/@shared/helpers/grid';
   styleUrls: ['./page-layout.component.scss'],
 })
 export class PageLayoutComponent implements OnInit {
+  @Input() pageTitle = 'Espn Companion';
+
   // eslint-disable-next-line @typescript-eslint/naming-convention
   readonly GRID_TEMPLATE = GRID_TEMPLATE;
 
