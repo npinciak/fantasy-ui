86bfb3fa5c0453e88b878ed338d91714c45d693d
npinciak <natepinciak@live.com>
2021-07-22 17:19:58 -0400

    Create a new lineup card for players

---
+++ src/app/espn/mlb/components/lineup-card-player/lineup-card-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BaseballPlayer } from '../../class/player.class';
+
+@Component({
+  selector: 'app-lineup-card-player',
+  templateUrl: './lineup-card-player.component.html',
+  styleUrls: ['./lineup-card-player.component.scss'],
+})
+export class LineupCardPlayerComponent implements OnInit {
+  @Input() player: BaseballPlayer;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
