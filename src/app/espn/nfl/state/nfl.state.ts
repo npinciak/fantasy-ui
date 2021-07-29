791eefcac410f3fb7f26a8fa628ccc2f31196327
npinciak <natepinciak@live.com>
2021-07-29 14:59:49 -0400

    Add new nfl service and add homepage to espn module

--- src/app/espn/nfl/state/nfl.state.ts
+++ src/app/espn/nfl/state/nfl.state.ts
@@ -3,6 +3,10 @@ import { State, Action, Selector, StateContext } from '@ngxs/store';
 
 import { EspnService } from '@espn/espn.service';
 import { NflStateModel } from './nfl.state.model';
+import { FetchFootballLeague } from '../actions/nfl.actions';
+import { entityMap } from '@app/@shared/operators';
+import { tap } from 'rxjs/operators';
+import { NflService } from '../services/nfl.service';
 @State<NflStateModel>({
   name: 'nfl',
   defaults: {
@@ -15,5 +19,20 @@ import { NflStateModel } from './nfl.state.model';
 })
 @Injectable()
 export class NflState {
-  constructor(private espnService: EspnService) {}
+  constructor(private nflService: NflService) {}
+
+  @Selector()
+  static scoringPeriod(state: NflStateModel) {
+    return state.scoringPeriodId;
+  }
+
+  @Action(FetchFootballLeague)
+  footballLeague(ctx: StateContext<NflStateModel>, { leagueId }: FetchFootballLeague) {
+    if (NflState.scoringPeriod !== null) {
+      console.log(`League ${leagueId} already in state, retrieving cache`);
+      return;
+    }
+
+    return this.nflService.fetchFootballLeague(leagueId);
+  }
 }
