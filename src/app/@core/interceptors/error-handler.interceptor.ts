44d06bd41e4aae31dedb9f14c2a2dd5fed1b45c2
npinciak <natepinciak@live.com>
2021-11-16 18:03:31 -0500

    Update http error handler messaging

--- src/app/@core/interceptors/error-handler.interceptor.ts
+++ src/app/@core/interceptors/error-handler.interceptor.ts
@@ -14,18 +14,7 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
 
   private errorHandler(response: HttpErrorResponse): Observable<HttpEvent<any>> {
     const code = response.status || 0;
-    const statusText = response.statusText;
-    let message = '';
-
-    switch (code) {
-      case 0:
-        message = 'Could not contact server';
-        break;
-
-      default:
-        message = response.error.message;
-        break;
-    }
+    const message = statusCodeToMessage[code];
 
     this.snackBar.open(`${code}: ${message}`, 'Close', {
       panelClass: ['mat-toolbar', 'mat-warn', 'text-white'],
@@ -38,11 +27,25 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
 export enum ErrorStatusCode {
   Unknown = 0,
   BadRequest = 400,
-  Unauthorized = 401,
+  Unauthorized,
   Forbidden = 403,
+  NotFound,
   NotAcceptable = 406,
   InternalServerError = 500,
   BadGateway = 502,
-  ServiceUnavailable = 503,
-  GatewayTimeout = 504,
+  ServiceUnavailable,
+  GatewayTimeout,
 }
+
+const statusCodeToMessage: { [key in ErrorStatusCode]: string } = {
+  [ErrorStatusCode.Unknown]: 'Could not contact server',
+  [ErrorStatusCode.BadRequest]: 'Bad Request',
+  [ErrorStatusCode.Unauthorized]: 'Unauthorized',
+  [ErrorStatusCode.Forbidden]: 'Forbidden',
+  [ErrorStatusCode.NotFound]: 'Resource Not Found',
+  [ErrorStatusCode.NotAcceptable]: 'Not Acceptable',
+  [ErrorStatusCode.InternalServerError]: 'Internal Server Error',
+  [ErrorStatusCode.BadGateway]: 'Bad Gateway',
+  [ErrorStatusCode.ServiceUnavailable]: 'Service Unavailable',
+  [ErrorStatusCode.GatewayTimeout]: 'Gateway Timeout',
+};
