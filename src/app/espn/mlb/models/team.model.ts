c6ec07003e81323218f5971c74ee240b7de881cd
npinciak <natepinciak@live.com>
2021-11-17 19:00:39 -0500

    Convert Team and BaseballTeam class to new type

---
+++ src/app/espn/mlb/models/team.model.ts
@@ -0,0 +1,10 @@
+export interface Team {
+  id: string;
+  name: string;
+  abbrev: string;
+  logo: string;
+  roster: string;
+  totalPoints: string;
+  currentRank: string;
+  rankDiff: string;
+}
