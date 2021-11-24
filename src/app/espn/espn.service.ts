daac8a1ac80034e6236889b3c5c067e9dae5ae12
npinciak <natepinciak@live.com>
2021-11-24 13:04:04 -0500

    Transform `espn.service` to be a gateway for other sports

--- src/app/espn/espn.service.ts
+++ src/app/espn/espn.service.ts
@@ -1,14 +1,15 @@
 import { HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { forkJoin, of } from 'rxjs';
 import { currentDate } from 'src/app/@shared/helpers/date';
 
 import { ApiService } from 'src/app/@shared/services/api.service';
-import { EspnClientEventList, EspnClientLeague } from '@mlb/interface';
+import { EspnClientEventList, EspnClientLeague } from './espn-client.model';
 
 export enum Sports {
   baseball = 'flb',
   football = 'ffl',
+  basketball = 'fba',
+  hockey = 'fhl',
 }
 
 @Injectable({
@@ -20,46 +21,35 @@ export class EspnService {
 
   constructor(private api: ApiService) {}
 
-  fetchEspnBaseball = (leagueId: number) => {
-    const $fantasyLeague = this._baseballLeague(leagueId);
-    const $games = this._baseballEvents();
-    return forkJoin([$fantasyLeague, $games]);
-  };
-
   updateTeam = (payload: unknown, leagueId: number) =>
     this.api.post<any>(`${this.fantasyBase}/games/flb/seasons/2021/segments/0/leagues/${leagueId}/transactions`, payload, {
       withCredentials: true,
       headers: this.postHeaders,
     });
 
-  playerNews = (days: number, playerId: number) => this._baseballPlayerNews(days, playerId);
-
-  freeAgents = (leagueId: number, headers: HttpHeaders) => this._baseballFreeAgents(leagueId, headers);
-
   /**
-   * Retrieve league information
+   * Retrieve Espn Fantasy League
    *
-   * @param leagueId League Id
-   * @returns League object
+   * @param leagueId
+   * @param sport
+   * @returns
    */
-  private readonly _baseballLeague = (leagueId: number) =>
-    this.api.get<EspnClientLeague>(
-      `${this.fantasyBase}/games/${Sports.baseball}/seasons/${this.currentYear}/segments/0/leagues/${leagueId}`,
-      {
-        params: this.params,
-      }
-    );
+  espnFantasyLeagueBySport = (leagueId: number, sport: Sports) =>
+    this.api.get<EspnClientLeague>(`${this.fantasyBase}/games/${sport}/seasons/${this.currentYear}/segments/0/leagues/${leagueId}`, {
+      params: this.params,
+    });
 
   /**
    * Fetch player news
    *
-   * @param days Days back for news
+   * @param numDays Days back for news
    * @param playerId Player Id
+   * @param sport
    * @returns Player news
    */
-  private readonly _baseballPlayerNews = (days: number, playerId: number) =>
-    this.api.get<any>(`${this.apiBase}/fantasy/v2/games/${Sports.baseball}/news/players`, {
-      params: new HttpParams().set('days', days.toString()).set('playerId', playerId.toString()),
+  espnFantasyPlayerNewsBySport = (numDays: number, playerId: number, sport: Sports) =>
+    this.api.get<any>(`${this.apiBase}/fantasy/v2/games/${sport}/news/players`, {
+      params: new HttpParams().set('days', numDays.toString()).set('playerId', playerId.toString()),
     });
 
   /**
@@ -68,11 +58,12 @@ export class EspnService {
    * @description Filter players via HttpHeader
    *
    * @param leagueId League Id
+   * @param sport
    * @param headers 'X-Fantasy-Filter' header required
    * @returns List of free agents
    */
-  private readonly _baseballFreeAgents = (leagueId: number, headers: HttpHeaders) =>
-    this.api.get<any>(`${this.fantasyBase}/games/${Sports.baseball}/seasons/${this.currentYear}/segments/0/leagues/${leagueId}`, {
+  espnFantasyFreeAgentsBySport = (leagueId: number, sport: Sports, headers: HttpHeaders) =>
+    this.api.get<any>(`${this.fantasyBase}/games/${sport}/seasons/${this.currentYear}/segments/0/leagues/${leagueId}`, {
       params: this.faParams,
       headers,
     });
@@ -81,11 +72,14 @@ export class EspnService {
    * Retrieve games for current date
    *
    * @description Fetches espn fantasy api for current games for today
+   *
+   * @param sport
+   *
    * @returns list of events
    */
-  private readonly _baseballEvents = () =>
-    this.api.get<EspnClientEventList>(`${this.apiBase}/fantasy/v2/games/${Sports.baseball}/games`, {
-      params: this.baseballEventParams,
+  espnFantasyEventsBySport = (sport: Sports) =>
+    this.api.get<EspnClientEventList>(`${this.apiBase}/fantasy/v2/games/${sport}/games`, {
+      params: this.espnEventParams,
     });
 
   /**
@@ -100,11 +94,10 @@ export class EspnService {
   /**
    * @todo
    */
-  private get baseballEventParams() {
+  private get espnEventParams() {
     let params = new HttpParams();
     params = params.append('useMap', 'true');
-    params = params.append('dates', currentDate());
-    // params = params.append('pbpOnly', 'true');
+    params = params.append('dates', '20211125'); // currentDate());
     return params;
   }
 
@@ -128,11 +121,6 @@ export class EspnService {
     params = params.append('view', 'mRoster');
     params = params.append('view', 'mScoreboard');
     params = params.append('view', 'mTeam');
-
-    // params = params.append('view', 'mPendingTransactions');
-    // params = params.append('scoringPeriodId','');
-    // params = params.append('view', 'mStatus');
-    // params = params.append('view', 'mTransactions2');
     return params;
   }
 
