48a67c7d5d9b36b65f8679ea4cb67cf87b4221e2
npinciak <natepinciak@live.com>
2021-12-22 12:53:39 -0500

    Create `enum-as-list` helpers

---
+++ src/app/@shared/helpers/enum-as-list.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+export function enumAsList<T extends string>(_enum: { [key: string]: T }): T[];
+export function enumAsList<T, U extends T[keyof T] & number>(_enum: U extends Exclude<U, string> ? T : never): U[];
+export function enumAsList<T, U extends T[keyof T] & (string | number)>(_enum: U): U[] {
+  const values = Object.values(_enum);
+  return values.some(o => typeof o === 'number') ? values.filter(o => !Number.isNaN(parseFloat(o.toString()))) : values;
+}
