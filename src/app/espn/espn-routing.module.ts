5f7cca0181b6228ba95dbb4fee848ef1d1c06681
npinciak <natepinciak@live.com>
2021-12-29 14:15:14 -0500

    Update routing to accomodate `leaguId`

--- src/app/espn/espn-routing.module.ts
+++ src/app/espn/espn-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UrlFragments } from '@app/@shared/url-builder';
+import { UrlFragments, UrlParams } from '@app/@shared/url-builder';
+import { HomeComponent as NflHomeComponent } from './nfl/pages/home/home.component';
+import { NFLLeagueResolver } from './nfl/resolvers/nfl-league.resolver';
 import { EspnHomeComponent } from './pages/espn-home/espn-home.component';
 import { EspnResolver } from './resolvers/espn.resolver';
 
@@ -9,6 +11,20 @@ export const routes: Routes = [
     path: UrlFragments.Empty,
     component: EspnHomeComponent,
     resolve: [EspnResolver],
+  },
+  {
+    path: UrlFragments.NFL,
+    children: [
+      {
+        path: UrlFragments.Empty,
+        component: NflHomeComponent,
+      },
+      {
+        path: UrlParams.LeagueId,
+        component: NflHomeComponent,
+        resolve: [NFLLeagueResolver],
+      },
+    ],
   },
   { path: '**', redirectTo: UrlFragments.Empty, pathMatch: 'full' },
 ];
