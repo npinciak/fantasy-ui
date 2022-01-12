1b7e7ede1fb52dc91e849667a015543275e1fce3
npinciak <natepinciak@live.com>
2022-01-12 18:44:29 -0500

    Create new model and selectors for tables

---
+++ src/app/dfs/selectors/daily-fantasy-table.selector.ts
@@ -0,0 +1,15 @@
+import { cellDataAccessor } from '@app/@shared/helpers/utils';
+import { Selector } from '@ngxs/store';
+import { TableColumn } from '../models/table.model';
+
+export class DailyFantasyTableSelectors {
+  @Selector()
+  static temp() {
+    return;
+  }
+
+  @Selector()
+  static tableColumns(): TableColumn[] {
+    return [];
+  }
+}
