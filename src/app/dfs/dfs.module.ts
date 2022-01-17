f198e6d3469477e2417896799e2b42343ecd2f3f
npinciak <natepinciak@live.com>
2022-01-17 17:26:59 -0500

    Remove old mlb and dfs components and imports

--- src/app/dfs/dfs.module.ts
+++ src/app/dfs/dfs.module.ts
@@ -5,28 +5,27 @@ import { FormsModule } from '@angular/forms';
 import { SharedModule } from '@app/@shared/shared.module';
 import { MaterialModule } from '@app/material.module';
 import { NgxsModule } from '@ngxs/store';
-import { PlayerTableComponent } from './components/player-table/player-table.component';
 import { SlateListComponent } from './components/slate-list/slate-list.component';
 import { TeamMatchupTableComponent } from './components/team-matchup-table/team-matchup-table.component';
 import { DfsRoutingModule } from './dfs-routing.module';
-import { HomeComponent as MLBHomeComponent } from './mlb/pages/home/home.component';
 import { MatchupTableComponent } from './nfl/components/matchup-table/matchup-table.component';
 import { PlayerTableRankingColumnComponent } from './nfl/components/player-table-ranking-column/player-table-ranking-column.component';
 import { PlayerTableComponent as NFLPlayerTableHomeComponent } from './nfl/components/player-table/player-table.component';
 import { HomeComponent as NFLHomeComponent } from './nfl/pages/home/home.component';
 
 import { HomeComponent as DfsHomeComponent } from './pages/home/home.component';
-import { DailyFantasyPlayersState } from './state/daily-fantasy-players.state';
-import { DailyFantasyScheduleState } from './state/daily-fantasy-schedule.state';
-import { DailyFantasySlateAttrState } from './state/daily-fantasy-slate-attr.state';
-import { DailyFantasySlateState } from './state/daily-fantasy-slate.state';
-import { DailyFantasyTeamsState } from './state/daily-fantasy-team.state';
 import { NflDfsLineupState } from './nfl/state/nfl-dfs-lineup.state';
 import { NflDfsPlayerMasterState } from './nfl/state/nfl-dfs-player-master.state';
 import { NflDfsPlayerSlateState } from './nfl/state/nfl-dfs-player-slate.state';
 import { NflDfsProfilerState } from './nfl/state/nfl-dfs-profiler.state';
 import { NflDfsTeamSlateState } from './nfl/state/nfl-dfs-team-slate.state';
 import { NflDfsState } from './nfl/state/nfl-dfs.state';
+import { HomeComponent as DfsHomeComponent } from './pages/home/home.component';
+import { DailyFantasyPlayersState } from './state/daily-fantasy-players.state';
+import { DailyFantasyScheduleState } from './state/daily-fantasy-schedule.state';
+import { DailyFantasySlateAttrState } from './state/daily-fantasy-slate-attr.state';
+import { DailyFantasySlateState } from './state/daily-fantasy-slate.state';
+import { DailyFantasyTeamsState } from './state/daily-fantasy-team.state';
 
 const states = [
   DailyFantasyPlayersState,
@@ -43,8 +42,6 @@ const states = [
 ];
 @NgModule({
   declarations: [
-    MLBHomeComponent,
-    PlayerTableComponent,
     TeamMatchupTableComponent,
     SlateListComponent,
     NFLHomeComponent,
