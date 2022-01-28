1389254ac99b402a025f34f29acad0deaa6e6bb8
npinciak <natepinciak@live.com>
2022-01-28 11:04:25 -0500

    Add environment files

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  dfsSource: 'rotogrinders',
+  dailyFantasyBase: 'https://rotogrinders.com',
+  dfsAws: 's3.amazonaws.com',
+  dfsJson: 'json.rotogrinders.com',
+  espnFantasyBaseballLeague: 1209434861,
+  espnFantasyFootballLeague: 1209434861,
+  espnFantasyBaseV2: 'https://site.api.espn.com/apis/fantasy/v2',
+  espnFantasyBaseV3: 'https://fantasy.espn.com/apis/v3',
+  espnFastcastBase: 'https://fcast.espncdn.com/FastcastService/pubsub/profiles/12000/topic/event-topevents/message',
+  espnWebsocketHost: 'https://fastcast.semfs.engsvc.go.com/public/websockethost',
+  espnOneFeed: 'https://onefeed.fan.api.espn.com/apis/v3/contentEngine',
+};
