311b69d7794f4821bad525023654442cd2a62c9a
npinciak <natepinciak@live.com>
2022-02-17 18:28:18 -0500

    Remove nullish coalescing operators

--- src/app/espn/mlb/services/mlb.service.ts
+++ src/app/espn/mlb/services/mlb.service.ts
@@ -14,7 +14,7 @@ import { map } from 'rxjs/operators';
 import { MLB_LINEUP_MAP } from '../consts/lineup.const';
 import { MLB_POSITION_MAP } from '../consts/position.const';
 import { MLB_TEAM_MAP } from '../consts/team.const';
-import { isPitcher, statsKeyMap } from '../helpers';
+import { isPitcher } from '../helpers';
 import { BaseballPlayer } from '../models/baseball-player.model';
 import { BaseballTeam } from '../models/baseball-team.model';
 
@@ -42,25 +42,25 @@ export class MlbService {
       id: player.playerId.toString(),
       name: player.playerPoolEntry?.player.fullName,
       img: playerImgBuilder(player.playerId, 'mlb'),
-      team: MLB_TEAM_MAP[player.playerPoolEntry?.player.proTeamId] ?? null,
-      position: MLB_POSITION_MAP[player.playerPoolEntry?.player.defaultPositionId].abbrev ?? null,
-      isInjured: player.playerPoolEntry?.player.injured ?? null,
-      injuryStatus: player.playerPoolEntry?.player.injuryStatus ?? null,
-      playerRatings: player.playerPoolEntry?.ratings ?? null,
-      playerOwnershipChange: player.playerPoolEntry?.player.ownership?.percentChange ?? null,
-      playerOwnershipPercentOwned: player.playerPoolEntry?.player.ownership?.percentOwned ?? null,
-      isPitcher: isPitcher(player.playerPoolEntry?.player.eligibleSlots) ?? null,
-      stats: MlbService.flattenPlayerStats(player.playerPoolEntry?.player.stats) ?? null,
-      lineupSlotId: player.lineupSlotId ?? null,
+      team: MLB_TEAM_MAP[player.playerPoolEntry?.player.proTeamId],
+      position: MLB_POSITION_MAP[player.playerPoolEntry?.player.defaultPositionId].abbrev,
+      isInjured: player.playerPoolEntry?.player.injured,
+      injuryStatus: player.playerPoolEntry?.player.injuryStatus,
+      playerRatings: player.playerPoolEntry?.ratings,
+      playerOwnershipChange: player.playerPoolEntry?.player.ownership?.percentChange,
+      playerOwnershipPercentOwned: player.playerPoolEntry?.player.ownership?.percentOwned,
+      isPitcher: isPitcher(player.playerPoolEntry?.player.eligibleSlots),
+      stats: MlbService.flattenPlayerStats(player.playerPoolEntry?.player.stats),
+      lineupSlotId: player.lineupSlotId,
       isStarting: false,
       startingStatus: null,
-      lineupSlot: MLB_LINEUP_MAP[player.lineupSlotId].abbrev ?? null,
+      lineupSlot: MLB_LINEUP_MAP[player.lineupSlotId].abbrev,
     }));
   }
 
   static flattenPlayerStats(stats: EspnClientPlayerStatsYear[]): EspnClientPlayerStatsEntityMap {
     return stats?.reduce((obj, val) => {
-      obj[val.id] = statsKeyMap(val.stats);
+      obj[val.id] = val.stats;
       return obj;
     }, {} as EspnClientPlayerStatsEntityMap);
   }
@@ -69,16 +69,16 @@ export class MlbService {
     return players.map(player => ({
       id: player.id.toString(),
       name: player.player.fullName,
-      img: playerImgBuilder(player.id, 'mlb') ?? null,
-      team: MLB_TEAM_MAP[player.player.proTeamId] ?? null,
-      position: MLB_POSITION_MAP[player.player.defaultPositionId].abbrev ?? null,
-      isInjured: player.player.injured ?? null,
-      injuryStatus: player.player.injuryStatus ?? null,
-      playerRatings: player.ratings ?? null,
-      playerOwnershipChange: player.player.ownership?.percentChange ?? null,
-      playerOwnershipPercentOwned: player.player.ownership?.percentOwned ?? null,
+      img: playerImgBuilder(player.id, 'mlb'),
+      team: MLB_TEAM_MAP[player.player.proTeamId],
+      position: MLB_POSITION_MAP[player.player.defaultPositionId].abbrev,
+      isInjured: player.player.injured,
+      injuryStatus: player.player.injuryStatus,
+      playerRatings: player.ratings,
+      playerOwnershipChange: player.player.ownership?.percentChange,
+      playerOwnershipPercentOwned: player.player.ownership?.percentOwned,
       isPitcher: isPitcher(player.player.eligibleSlots),
-      stats: MlbService.flattenPlayerStats(player.player.stats) ?? null,
+      stats: MlbService.flattenPlayerStats(player.player.stats),
       lineupSlotId: null,
       isStarting: false,
       startingStatus: null,
