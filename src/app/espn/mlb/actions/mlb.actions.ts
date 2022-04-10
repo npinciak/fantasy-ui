c8445e5b0d5011efd09bde7f53f02c6978ecc8c9
npinciak <natepinciak@live.com>
2022-04-10 10:26:09 -0400

    Add seasonId and scoringPeriodId to `fantasy-baseball-league.state.ts`

--- src/app/espn/mlb/actions/mlb.actions.ts
+++ src/app/espn/mlb/actions/mlb.actions.ts
@@ -1,4 +1,14 @@
 export class FetchBaseballLeague {
-  public static readonly type = `[fantasyBaseballLeague] Fetch Baseball League`;
+  public static readonly type = `[fantasyBaseballLeague] FetchBaseballLeague`;
   constructor(public payload: { leagueId: number }) {}
+}
+
+export class PatchSeasonId {
+  public static readonly type = `[fantasyBaseballLeague] PatchSeasonId`;
+  constructor(public payload: { seasonId: string }) {}
+}
+
+export class PatchScoringPeriodId {
+  public static readonly type = `[fantasyBaseballLeague] PatchScoringPeriodId`;
+  constructor(public payload: { scoringPeriodId: number }) {}
 }
