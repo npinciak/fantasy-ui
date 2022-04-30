acb86a24522f700d25c45ab37a087f092aef8b10
npinciak <natepinciak@live.com>
2022-04-30 10:51:50 -0400

    Update datavis component with plotly

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@app/material.module';
+import { PlotlyModule } from 'angular-plotly.js';
+import * as PlotlyJS from 'plotly.js-dist-min';
 import { DataVisComponent } from './components/data-vis/data-vis.component';
 import { DropdownFilterComponent } from './components/dropdown-filter/dropdown-filter.component';
 import { NoDataComponent } from './components/no-data/no-data.component';
 import { ScrollableTableComponent } from './components/scrollable-table/scrollable-table.component';
 import { StickyTableCellComponent } from './components/sticky-table-cell/sticky-table-cell.component';
 import { MlbStatPipe } from './pipes/mlb-stat.pipe';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
@@ -19,7 +23,7 @@ import { MlbStatPipe } from './pipes/mlb-stat.pipe';
     DataVisComponent,
     MlbStatPipe,
   ],
-  imports: [RouterModule, MaterialModule, FlexLayoutModule, CommonModule],
+  imports: [PlotlyModule, RouterModule, MaterialModule, FlexLayoutModule, CommonModule],
   exports: [MlbStatPipe, DataVisComponent, NoDataComponent, ScrollableTableComponent, StickyTableCellComponent, DropdownFilterComponent],
 })
 export class SharedModule {}
