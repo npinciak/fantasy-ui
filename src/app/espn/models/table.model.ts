04acca271f7f08098eb50dd2f07df88a23778106
npinciak <natepinciak@live.com>
2022-05-25 17:18:40 -0400

    Move and change table selectors to consts

--- src/app/espn/models/table.model.ts
+++ src/app/espn/models/table.model.ts
@@ -1,3 +1,5 @@
+import { cellDataAccessor } from '@app/@shared/helpers/utils';
+
 export type TableColumn = {
   columnDef: string;
   headerCell: string;
@@ -10,8 +12,16 @@ export enum TableColumnDataType {
   String,
 }
 
-export interface BaseTableRow {
+export interface BaseTableRow<T> {
   columnDef: string;
-  cellData: (data: any) => any;
+  cellData: (data: T) => T;
   headerLabel: string;
+}
+
+export function transformTableColumnsToTableRows(cols: TableColumn[]): BaseTableRow<TableColumn>[] {
+  return cols.map(col => ({
+    columnDef: col.columnDef,
+    cellData: data => cellDataAccessor(data, col.columnDef),
+    headerLabel: col.headerLabel,
+  }));
 }
