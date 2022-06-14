02d2fe8476a22449ebba73c7c8a8f476dff3742c
npinciak <natepinciak@live.com>
2022-06-14 18:18:59 -0400

    Remove unused helpers

--- src/app/@shared/helpers/toInt.ts
+++
@@ -1,9 +0,0 @@
-export const toInt = (str: string): { valid: boolean; int?: number } => {
-  // eslint-disable-next-line radix
-  const int = parseInt(str);
-  if (isNaN(int)) {
-    return { valid: false };
-  } else {
-    return { valid: true, int };
-  }
-};
--- src/app/espn/mlb/models/baseball-league.model.mock.ts
+++
@@ -1,5 +0,0 @@
-import { MOCK_BASEBALL_TEAM_1 } from './baseball-team.model.mock';
-
-export const MOCK_BASEBALL_LEAGUE = {
-  teams: [MOCK_BASEBALL_TEAM_1],
-};
