ba3e0365306c3d39a736693cc81579fd66957ff5
npinciak <natepinciak@live.com>
2022-10-22 17:07:19 -0400

    Simplify `espn.service` transformers

--- src/app/espn/models/player.model.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Base player model
- */
-type PlayerProperties = 'id' | 'name' | 'img' | 'team' | 'teamId' | 'teamUid' | 'position';
-
-export type Player = { [prop in PlayerProperties]: string };
--- src/app/espn/models/position.model.ts
+++
@@ -1,7 +0,0 @@
-interface BasePositionProperties {
-  abbrev: string;
-  name: string;
-}
-
-export type Position = BasePositionProperties;
-export type PositionMap = Record<number, Position>;
