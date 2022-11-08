362f8e5f526c18a71eff8e0fc279baf7ef59dc67
npinciak <natepinciak@live.com>
2022-11-08 10:02:04 -0500

    Fix imports

--- src/app/sports-ui/facades/sports-ui-league-form.facade.ts
+++ src/app/sports-ui/facades/sports-ui-league-form.facade.ts
@@ -3,8 +3,8 @@ import { select } from '@app/@shared/models/typed-select';
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { FantasySports } from '../../espn/models/espn-endpoint-builder.model';
+import { SportsUiLeagueForm } from '../actions/sports-ui-league-form.actions';
 import { SportsUiLeagueFormSelectors } from '../selectors/sports-ui-league-form.selectors';
-import { Reset, SetLeagueIdValue, SetLeagueSportValue, Submit } from '../state/sports-ui-league-form.state';
 
 @Injectable({
   providedIn: 'root',
@@ -19,18 +19,18 @@ export class SportsUiLeagueFormFacade {
   constructor(private store: Store) {}
 
   setLeagueId(leagueId: string): Observable<void> {
-    return this.store.dispatch(new SetLeagueIdValue({ leagueId }));
+    return this.store.dispatch(new SportsUiLeagueForm.SetLeagueIdValue({ leagueId }));
   }
 
   setSport(leagueSport: FantasySports): Observable<void> {
-    return this.store.dispatch(new SetLeagueSportValue({ leagueSport }));
+    return this.store.dispatch(new SportsUiLeagueForm.SetLeagueSportValue({ leagueSport }));
   }
 
   reset(): Observable<void> {
-    return this.store.dispatch(new Reset());
+    return this.store.dispatch(new SportsUiLeagueForm.Reset());
   }
 
   submit(): Observable<void> {
-    return this.store.dispatch(new Submit());
+    return this.store.dispatch(new SportsUiLeagueForm.Submit());
   }
 }
