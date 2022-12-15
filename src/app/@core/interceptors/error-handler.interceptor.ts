74a9b217aec68ba744f8df939270a171b25473b7
npinciak <natepinciak@live.com>
2022-12-15 16:40:18 -0500

    Move error handler models to own file

--- src/app/@core/interceptors/error-handler.interceptor.ts
+++ src/app/@core/interceptors/error-handler.interceptor.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { exists } from '@app/@shared/helpers/utils';
+import { ErrorStatusCode, statusCodeToEspnMessage, statusCodeToMessage } from '@app/@shared/models/http-errors.model';
 import { FANTASY_BASE_V3, FASTCAST_BASE } from '@app/espn/espn.const';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -39,48 +40,3 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
     throw response;
   }
 }
-
-export enum StatusCode {
-  Ok = 200,
-  NoContent = 204,
-  BadRequest = 400,
-}
-
-export enum ErrorStatusCode {
-  Unknown = 0,
-  BadRequest = 400,
-  Unauthorized,
-  Forbidden = 403,
-  NotFound,
-  NotAcceptable = 406,
-  InternalServerError = 500,
-  BadGateway = 502,
-  ServiceUnavailable,
-  GatewayTimeout,
-}
-
-const statusCodeToEspnMessage: { [key in ErrorStatusCode]: string } = {
-  [ErrorStatusCode.Unknown]: 'Invalid LeagueId',
-  [ErrorStatusCode.BadRequest]: 'Bad Request',
-  [ErrorStatusCode.Unauthorized]: 'Unauthorized, not a public league',
-  [ErrorStatusCode.Forbidden]: 'Forbidden',
-  [ErrorStatusCode.NotFound]: 'League does not exist',
-  [ErrorStatusCode.NotAcceptable]: 'Not Acceptable',
-  [ErrorStatusCode.InternalServerError]: 'Internal Server Error',
-  [ErrorStatusCode.BadGateway]: 'Bad Gateway',
-  [ErrorStatusCode.ServiceUnavailable]: 'Service Unavailable',
-  [ErrorStatusCode.GatewayTimeout]: 'Gateway Timeout',
-};
-
-const statusCodeToMessage: { [key in ErrorStatusCode]: string } = {
-  [ErrorStatusCode.Unknown]: 'Could not contact server',
-  [ErrorStatusCode.BadRequest]: 'Bad Request',
-  [ErrorStatusCode.Unauthorized]: 'Unauthorized',
-  [ErrorStatusCode.Forbidden]: 'Forbidden',
-  [ErrorStatusCode.NotFound]: 'Resource Not Found',
-  [ErrorStatusCode.NotAcceptable]: 'Not Acceptable',
-  [ErrorStatusCode.InternalServerError]: 'Internal Server Error',
-  [ErrorStatusCode.BadGateway]: 'Bad Gateway',
-  [ErrorStatusCode.ServiceUnavailable]: 'Service Unavailable',
-  [ErrorStatusCode.GatewayTimeout]: 'Gateway Timeout',
-};
