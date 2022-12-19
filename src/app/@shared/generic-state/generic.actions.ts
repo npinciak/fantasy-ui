28fef8a8e7eabd6787920f6c8e399ccec6b1734e
npinciak <natepinciak@live.com>
2022-12-19 17:57:14 -0500

    Added new Generic actions handler for AddOrUpdate and ClearAndAdd actions

--- src/app/@shared/generic-state/generic.actions.ts
+++ src/app/@shared/generic-state/generic.actions.ts
@@ -3,7 +3,32 @@ export interface GenericPayloadActionClass<T> {
   new (payload: T[]): { payload: T[] };
 }
 
-export interface GenericPayloadClearActionClass<T> {
+export interface GenericPayloadClearActionClass {
   type: string;
   new (): unknown;
+}
+
+export function GenericActions<T>({ stateName }: { stateName: string }): IGenericActionsClass<T> {
+  class GenericActionsClass {
+    static readonly stateName = stateName;
+
+    static AddOrUpdate = class {
+      public static readonly type = `[${stateName}] AddOrUpdate`;
+      constructor(public payload: T[]) {}
+    };
+
+    static ClearAndAdd = class {
+      public static readonly type = `[${stateName}] ClearAndAdd`;
+      constructor(public payload: T[]) {}
+    };
+  }
+
+  return GenericActionsClass;
+}
+
+interface IGenericActionsClass<T> {
+  new (...args: any[]): any;
+  stateName: string;
+  AddOrUpdate: GenericPayloadActionClass<T>;
+  ClearAndAdd: GenericPayloadActionClass<T>;
 }
