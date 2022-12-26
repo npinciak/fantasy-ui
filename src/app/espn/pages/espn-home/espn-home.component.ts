b7be3be50749bd32b8c4695a35641db3343495ea
npinciak <natepinciak@live.com>
2022-12-26 17:24:52 -0500

    Update styles for some espn components

--- src/app/espn/pages/espn-home/espn-home.component.ts
+++ src/app/espn/pages/espn-home/espn-home.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterFacade } from '@app/@core/store/router/router.facade';
-import { UrlFragments } from '@app/@core/store/router/url-builder';
-import { LayoutService } from '@app/@shared/services/layout.service';
-import { SportsUiLeaguesFacade } from '@app/sports-ui/facades/sports-ui-leagues.facade';
 
 @Component({
   selector: 'app-espn-home',
@@ -10,29 +6,7 @@ import { SportsUiLeaguesFacade } from '@app/sports-ui/facades/sports-ui-leagues.
   styleUrls: ['./espn-home.component.scss'],
 })
 export class EspnHomeComponent implements OnInit {
-  isMobile$ = this.layoutService.isMobile$;
-  allLeagues$ = this.sportsUiLeaguesFacade.allLeagues$;
+  constructor() {}
 
-  constructor(
-    private layoutService: LayoutService,
-    private routerFacade: RouterFacade,
-    private sportsUiLeaguesFacade: SportsUiLeaguesFacade
-  ) {}
-
-  ngOnInit() {
-    this.sportsUiLeaguesFacade.fetchLeagues();
-  }
-
-  onNavigateLeague(val: { sport: UrlFragments; leagueId: string }) {
-    this.routerFacade.navigateToLeagueHome(val.sport, val.leagueId);
-  }
-
-  // TODO: Refactor me
-  onAddLeague(event: Record<string, { sport: string }>): void {
-    const key = Object.keys(event)[0];
-  }
-
-  onRemoveLeague(leagueId: string): void {
-    this.sportsUiLeaguesFacade.deleteLeague(leagueId);
-  }
+  ngOnInit() {}
 }
