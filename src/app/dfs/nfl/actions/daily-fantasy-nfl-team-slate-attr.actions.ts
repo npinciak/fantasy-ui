7d43112fa1560f36948851278c5eef71ec3853d9
Nathan Pinciak <natepinciak@live.com>
2022-12-26 17:53:37 -0500

    Fix filtering for dfs tables (#203)

--- src/app/dfs/nfl/actions/daily-fantasy-nfl-team-slate-attr.actions.ts
+++ src/app/dfs/nfl/actions/daily-fantasy-nfl-team-slate-attr.actions.ts
@@ -3,15 +3,15 @@ import { SlateTeam } from '@app/dfs/service/slate.service';
 export const name = 'dailyFantasyNflTeamSlateAttributes';
 
 export class SetNflTeamSlateAttributes {
-  static readonly type = `${name} SetNflTeamSlateAttributes`;
+  static readonly type = `[${name}] SetNflTeamSlateAttributes`;
   constructor(public payload: SlateTeam[]) {}
 }
 
 export class ClearAndAddNflTeamSlateAttributes {
-  static readonly type = `${name} ClearAndAddNflTeamSlateAttributes`;
+  static readonly type = `[${name}] ClearAndAddNflTeamSlateAttributes`;
   constructor(public payload: SlateTeam[]) {}
 }
 
 export class ClearNflTeamSlateAttributes {
-  static readonly type = `${name} ClearNflTeamSlateAttributes`;
+  static readonly type = `[${name}] ClearNflTeamSlateAttributes`;
 }
