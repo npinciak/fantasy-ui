e24404d12892a1b3db427652b7b452e80af89f56
npinciak <natepinciak@live.com>
2022-12-26 17:07:25 -0500

    Add new `espn-info-col` for players

--- src/app/espn/components/espn-stats-table/espn-stats-table.component.ts
+++ src/app/espn/components/espn-stats-table/espn-stats-table.component.ts
@@ -1,4 +1,14 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -7,6 +17,7 @@ import { cellDataAccessor } from '@app/@shared/helpers/utils';
 import { TableColumnDataType } from '@app/@shared/models/table-columns.model';
 import { BaseballPlayer } from '@app/espn/mlb/models/baseball-player.model';
 import { StatTypePeriodId } from '@app/espn/models/espn-stats.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-espn-stats-table',
@@ -31,6 +42,7 @@ export class EspnStatsTableComponent implements OnChanges, AfterViewInit {
   readonly TableColumnDataType = TableColumnDataType;
 
   dataSource: MatTableDataSource<BaseballPlayer>;
+  isLoading$ = new BehaviorSubject(false);
 
   constructor() {
     this.dataSource = new MatTableDataSource<BaseballPlayer>();
@@ -47,9 +59,15 @@ export class EspnStatsTableComponent implements OnChanges, AfterViewInit {
   }
 
   initTable(): void {
+    this.isLoading$.next(true);
+
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.dataSource.data = this.data;
     this.dataSource.sortingDataAccessor = (player, stat) => cellDataAccessor(player, stat);
+
+    if (this.dataSource.data.length > 0) {
+      this.isLoading$.next(false);
+    }
   }
 }
