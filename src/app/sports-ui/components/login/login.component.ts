3717bc5d150e1ad04e38a68b3d5cd2099eed9e76
npinciak <natepinciak@live.com>
2022-12-26 14:30:38 -0500

    Add `login` component

---
+++ src/app/sports-ui/components/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+})
+export class LoginComponent implements OnInit {
+  @Input() isFormValid: boolean = false;
+
+  @Output() signInClicked = new EventEmitter();
+  @Output() passwordInputChanged = new EventEmitter();
+  @Output() emailInputChanged = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onPasswordInputChange(val: string) {
+    this.passwordInputChanged.emit(val);
+  }
+
+  onEmailInputChange(val: string) {
+    this.emailInputChanged.emit(val);
+  }
+
+  onSignIn() {
+    this.signInClicked.emit();
+  }
+}
