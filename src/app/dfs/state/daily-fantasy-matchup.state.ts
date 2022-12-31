284ae7201f718f851ae523c537dd2d2951466646
Nathan Pinciak <natepinciak@live.com>
2022-12-31 17:38:05 -0500

    Update DailyFantasyMatchup to use new Generic Actions handler

--- src/app/dfs/state/daily-fantasy-matchup.state.ts
+++ src/app/dfs/state/daily-fantasy-matchup.state.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { GenericState } from '@app/@shared/generic-state/generic.state';
 import { State } from '@ngxs/store';
-import { ClearAndAddSchedule, SetSchedule } from '../actions/daily-fantasy-schedule.actions';
+import { DailyFantasyMatchups } from '../actions/daily-fantasy-matchup.actions';
 
-@State({ name: 'dailyFantasyMatchup' })
+@State({ name: DailyFantasyMatchups.stateName })
 @Injectable()
 export class DailyFantasyMatchupState extends GenericState({
   idProperty: 'id',
-  addOrUpdate: SetSchedule,
-  clearAndAdd: ClearAndAddSchedule,
+  addOrUpdate: DailyFantasyMatchups.AddOrUpdate,
+  clearAndAdd: DailyFantasyMatchups.ClearAndAdd,
 }) {}
