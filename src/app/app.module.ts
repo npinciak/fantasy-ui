d87cda0642c1d208496daf3491d78ee4a5c7dfb9
Nathan Pinciak <natepinciak@live.com>
2023-01-13 11:19:58 -0500

    Add state serialization for router state to prevent app from crashing using Redux DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-import { NgxsRouterPluginModule, RouterStateSerializer } from '@ngxs/router-plugin';
+import { RouterStateSerializer } from '@ngxs/router-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { environment } from 'src/environments/environment';
 import { AuthenticationModule } from './@core/authentication/authentication.module';
 import { httpInterceptorProviders } from './@core/interceptors';
 import { ShellModule } from './@core/shell/shell.module';
 import { CustomRouterStateSerializer } from './@core/store/router/router-state.serializer';
+import { RouterState } from './@core/store/router/router.state';
 import { SharedModule } from './@shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const actionSanitizer = (action: { action: any; payload: any; type: string; addedStates?: any }) => {
+  const uiRouterActions = /router+/g;
+  return uiRouterActions.test(action.type) ? { type: action.type, transition: sanitizeUIRouterTransition(action) } : action;
+};
+
+const stateSanitizer = (state): any => {
+  if (state.router && state.router.state) {
+    return {
+      ...state,
+      router: sanitizeUIRouterTransition(state.router.state),
+    };
+  }
+  return state;
+};
+
+const sanitizeUIRouterTransition = (routerState: any): any => {
+  const { params, data, url, queryParams } = routerState;
+  return {
+    queryParams,
+    params,
+    data,
+    url,
+  };
+};
+const reduxDevtoolsExtensionOptions = {
+  actionSanitizer,
+  stateSanitizer,
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,9 +51,9 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    NgxsRouterPluginModule.forRoot(),
-    NgxsModule.forRoot([], { developmentMode: !environment.production, selectorOptions: { injectContainerState: false } }),
-    NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production }),
+    // NgxsRouterPluginModule.forRoot(),
+    NgxsModule.forRoot([RouterState], { developmentMode: !environment.production, selectorOptions: { injectContainerState: false } }),
+    NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production, actionSanitizer, stateSanitizer }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
