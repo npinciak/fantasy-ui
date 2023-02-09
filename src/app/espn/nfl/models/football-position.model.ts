9d152ac8385b1b2aa5edeb681eaf6be5197cfca2
Nathan Pinciak <natepinciak@live.com>
2023-02-09 17:45:18 -0500

    Move football position list to `sports-ui-sdk`

--- src/app/espn/nfl/models/football-position.model.ts
+++ src/app/espn/nfl/models/football-position.model.ts
@@ -1,16 +1,7 @@
 import { FilterOptions } from '@app/@shared/models/filter.model';
-import { FootballPosition, NFL_POSITION_MAP } from 'sports-ui-sdk';
+import { FootballPosition, NFL_POSITION_LIST_DEFAULT, NFL_POSITION_MAP } from 'sports-ui-sdk';
 
-export const FOOTBALL_POSITION_LIST_DEFAULT = [
-  FootballPosition.QB,
-  FootballPosition.RB,
-  FootballPosition.WR,
-  FootballPosition.TE,
-  FootballPosition.K,
-  FootballPosition.DST,
-];
-
-export const FOOTBALL_POSITION_LIST_FILTER: FilterOptions<FootballPosition>[] = FOOTBALL_POSITION_LIST_DEFAULT.map(p => ({
+export const FOOTBALL_POSITION_LIST_FILTER: FilterOptions<FootballPosition>[] = NFL_POSITION_LIST_DEFAULT.map(p => ({
   label: NFL_POSITION_MAP[p].abbrev,
   value: p,
 }));
