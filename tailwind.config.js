33757c6e71cd6678615d13621bc90f9dd8c92e1b
Nathan Pinciak <natepinciak@live.com>
2023-02-14 10:53:42 -0500

    Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ['./src/**/*.component.{html,scss}', './src/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
