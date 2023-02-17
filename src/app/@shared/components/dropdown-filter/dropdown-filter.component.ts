d8642ff557035edb4dfc1de064f83be6e8cf14a8
Nathan Pinciak <natepinciak@live.com>
2023-02-17 18:28:22 -0500

    Add aria-labels to drop down, remove material dropdown instances

--- src/app/@shared/components/dropdown-filter/dropdown-filter.component.ts
+++ src/app/@shared/components/dropdown-filter/dropdown-filter.component.ts
@@ -5,10 +5,11 @@ import { LayoutService } from '@app/@shared/services/layout.service';
 @Component({
   selector: 'app-dropdown-filter',
   templateUrl: './dropdown-filter.component.html',
-  styleUrls: ['./dropdown-filter.component.scss'],
 })
 export class DropdownFilterComponent {
-  @Input() title: string;
+  @Input() title = '';
+  @Input() ariaLabel = '';
+
   @Input() selectedOption: number | string;
   @Input() filterItems: FilterOptions<number | string>[];
   @Input() disabled = false;
