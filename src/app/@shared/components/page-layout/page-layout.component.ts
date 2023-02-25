32c7cf5ab4b930fae0b9c0b3a5fa22d3d5126fe2
Nathan Pinciak <natepinciak@live.com>
2023-02-25 10:37:33 -0500

    Fix broken layouts

--- src/app/@shared/components/page-layout/page-layout.component.ts
+++ src/app/@shared/components/page-layout/page-layout.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-layout',
   template: `
-    <div class="flex flex-1 flex-col gap-5 md:flex-row">
-      <aside class="order-first md:w-80">
+    <div class="flex flex-1 flex-col gap-5 sm:flex-row">
+      <aside class="order-first sm:w-80">
         <ng-content select="[sidebar]"></ng-content>
       </aside>
 
