c9e5b6af241b49335eaf183e37cee85be405d4ba
Nathan Pinciak <natepinciak@live.com>
2023-03-23 11:13:18 -0400

    Cleanup layout line spacing

--- src/app/@shared/components/page-layout/page-layout.component.ts
+++ src/app/@shared/components/page-layout/page-layout.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-layout',
   template: `
-    <div class="flex flex-1 flex-col gap-5 px-4 py-6 sm:flex-row sm:px-0">
-      <aside class="order-first sm:w-80">
+    <div class="flex flex-1 flex-col gap-5 px-4 py-6 sm:flex-row sm:px-4 lg:px-6">
+      <nav class="order-first sm:w-80">
         <ng-content select="[sidebar]"></ng-content>
-      </aside>
-
+      </nav>
       <main class="flex-1">
         <ng-content select="[main]"></ng-content>
       </main>
