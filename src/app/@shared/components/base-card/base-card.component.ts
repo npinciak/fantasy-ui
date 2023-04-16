a626183492732ede1feaa6e9e70b1758b77df3b6
Nathan Pinciak <natepinciak@live.com>
2023-04-16 18:37:07 -0400

    Add dynamic height to base-cards

--- src/app/@shared/components/base-card/base-card.component.ts
+++ src/app/@shared/components/base-card/base-card.component.ts
@@ -1,17 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-base-card',
   templateUrl: './base-card.component.html',
 })
-export class BaseCardComponent implements OnInit {
-  @Input() title = 'Title';
-  @Input() subtitle = 'Subtitle';
+export class BaseCardComponent {
+  @Input() title = null;
+  @Input() subtitle = null;
   @Input() titleColor = 'text-orange-400';
   @Input() subTitleColor = 'text-orange-700';
   @Input() height = null;
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
