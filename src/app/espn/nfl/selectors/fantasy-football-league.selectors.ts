42ad88296291f8cfb4cf4343be7d4323bc0647cb
Nathan Pinciak <natepinciak@live.com>
2023-05-16 14:43:44 -0400

    Small fantasy football selectors and component.html changes

--- src/app/espn/nfl/selectors/fantasy-football-league.selectors.ts
+++ src/app/espn/nfl/selectors/fantasy-football-league.selectors.ts
@@ -4,7 +4,7 @@ import { FootballScoringPeriod } from '../fantasy-football-scoring-period';
 import { FantasyFootballLeagueState } from '../state/fantasy-football-league.state';
 
 export class FantasyFootballLeagueSelector extends FantasyLeagueBaseSelector(FantasyFootballLeagueState) {
-  @Selector([FantasyFootballLeagueSelector.slices.seasonId, FantasyFootballLeagueSelector.getScoringPeriodId])
+  @Selector([FantasyFootballLeagueSelector.slices.seasonId, FantasyFootballLeagueSelector.slices.scoringPeriodId])
   static scoringPeriodFilters(seasonId: string | null, week: string | null) {
     return [...FootballScoringPeriod.filterOptionList(seasonId)];
   }
