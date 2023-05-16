a504b1aec6f9845c78f988d5700f1978a329ac18
Nathan Pinciak <natepinciak@live.com>
2023-05-16 14:43:04 -0400

    Update SportsBook state to be `GenericState`

--- src/app/sportsbook/state/league-spread.state.ts
+++ src/app/sportsbook/state/league-spread.state.ts
@@ -1,31 +1,8 @@
 import { Injectable } from '@angular/core';
 import { GenericState } from '@app/@shared/generic-state/generic.state';
-import { exists } from '@app/@shared/utilities/utilities.m';
-import { Action, State, Store } from '@ngxs/store';
+import { State } from '@ngxs/store';
 import { SportsBookLeagueSpread } from '../actions/league-spread.actions';
-import { LEAGUE_ID } from '../isportgenius-endpoint-builder';
-import { SportsBookService } from '../service/sportsbook.service';
 
 @State({ name: SportsBookLeagueSpread.stateName })
 @Injectable()
-export class SportsBookLeagueSpreadState extends GenericState({ idProperty: 'team', actionHandler: SportsBookLeagueSpread }) {
-  constructor(private client: SportsBookService, private store: Store) {
-    super();
-  }
-
-  @Action(SportsBookLeagueSpread.Fetch)
-  async footballLeague() {
-    try {
-      const {
-        upcoming: { spread },
-      } = await this.client.leagueStats(LEAGUE_ID.NBA).toPromise();
-
-      if (!exists(spread)) {
-        console.log('spread value:', spread);
-        return;
-      }
-
-      this.store.dispatch([new SportsBookLeagueSpread.AddOrUpdate(spread)]);
-    } catch (error) {}
-  }
-}
+export class SportsBookLeagueStatsState extends GenericState({ idProperty: 'team', actionHandler: SportsBookLeagueSpread }) {}
