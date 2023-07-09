1e318d124346709ad16ed14cb675a37f7e260580
Nathan Pinciak <natepinciak@live.com>
2023-07-09 17:29:57 -0400

    Create new method to pick fields from object and return picked fields

---
+++ libs/ui-sdk/src/lib/helpers/pick-fields.ts
@@ -0,0 +1,26 @@
+/**
+ * Takes input of object and specified fields, returns specfied fields
+ *
+ * @param object
+ * @param fields
+ * @returns
+ *
+ * @example ```typescript
+ *
+ * pickFields(person, ["name", "email", "phone"])
+ *
+ * ```
+ *
+ */
+export function pickFields<T extends object, K extends keyof T>(object: T, fields: K[]): Pick<T, K> {
+  const result: Pick<T, K> = {} as Pick<T, K>;
+
+  for (const field of fields) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (object.hasOwnProperty(field)) {
+      result[field] = object[field];
+    }
+  }
+
+  return result;
+}
