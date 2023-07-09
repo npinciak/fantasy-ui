f7c3fa7b79a8bca0d1d090f76c2a2fa7c7848d93
Nathan Pinciak <natepinciak@live.com>
2023-07-09 17:55:20 -0400

    Optimize existsFilter

--- src/app/@shared/utilities/exists.ts
+++ src/app/@shared/utilities/exists.ts
@@ -3,9 +3,13 @@ export function exists<T>(value: NonNullable<T> | null | undefined): value is No
 }
 
 export function existsFilter<T>(value: (NonNullable<T> | null | undefined)[]): NonNullable<T>[] {
-  const result: NonNullable<T>[] = [];
-  value.forEach(o => {
-    if (exists(o)) result.push(o);
-  });
-  return result;
+  let index = 0;
+  for (let i = 0; i < value.length; i++) {
+    if (exists(value[i])) {
+      value[index] = value[i];
+      index++;
+    }
+  }
+  value.length = index;
+  return value as NonNullable<T>[];
 }
