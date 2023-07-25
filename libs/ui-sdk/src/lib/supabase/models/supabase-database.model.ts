f3b6547abcadc6c78c7a587cc7e9b322a63c59d4
Nathan Pinciak <natepinciak@live.com>
2023-07-25 11:36:44 -0400

    Cleanup and consolidate Supabase models

--- libs/ui-sdk/src/lib/supabase/models/supabase-database.model.ts
+++ libs/ui-sdk/src/lib/supabase/models/supabase-database.model.ts
@@ -1,137 +1,56 @@
 export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
+type FangraphsConstants = {
+  cFIP: number;
+  id: number;
+  'R/PA': number;
+  'R/W': number;
+  runCS: number;
+  runSB: number;
+  season: number;
+  w1B: number;
+  w2B: number;
+  w3B: number;
+  wBB: number;
+  wHBP: number;
+  wHR: number;
+  wOBA: number;
+  wOBAScale: number;
+};
+
+type League = {
+  created_at: string | null;
+  id: number;
+  leagueId: string;
+  name: string | null;
+  ownerId: string;
+  season: string | null;
+  sport: string | null;
+  expired: boolean;
+};
+
+type Team = {
+  created_at: string | null;
+  espnId: number;
+  id: number;
+  isFavorite: boolean | null;
+  leagueId: string | null;
+  name: string | null;
+  ownerId: string;
+};
+
+type Table<T> = {
+  Row: T;
+  Insert: Partial<T>;
+  Update: Partial<T>;
+};
+
 export interface Database {
   public: {
     Tables: {
-      'fangraphs-constants': {
-        Row: {
-          cFIP: number;
-          id: number;
-          'R/PA': number;
-          'R/W': number;
-          runCS: number;
-          runSB: number;
-          season: number;
-          w1B: number;
-          w2B: number;
-          w3B: number;
-          wBB: number;
-          wHBP: number;
-          wHR: number;
-          wOBA: number;
-          wOBAScale: number;
-        };
-        Insert: {
-          cFIP?: number;
-          id?: number;
-          'R/PA'?: number;
-          'R/W'?: number;
-          runCS?: number;
-          runSB?: number;
-          season: number;
-          w1B?: number;
-          w2B?: number;
-          w3B?: number;
-          wBB?: number;
-          wHBP?: number;
-          wHR?: number;
-          wOBA?: number;
-          wOBAScale?: number;
-        };
-        Update: {
-          cFIP?: number;
-          id?: number;
-          'R/PA'?: number;
-          'R/W'?: number;
-          runCS?: number;
-          runSB?: number;
-          season?: number;
-          w1B?: number;
-          w2B?: number;
-          w3B?: number;
-          wBB?: number;
-          wHBP?: number;
-          wHR?: number;
-          wOBA?: number;
-          wOBAScale?: number;
-        };
-      };
-      Leagues: {
-        Row: {
-          created_at: string | null;
-          id: number;
-          leagueId: string;
-          name: string | null;
-          ownerId: string;
-          season: string | null;
-          sport: string | null;
-          expired: boolean;
-        };
-        Insert: {
-          created_at?: string | null;
-          id?: number;
-          leagueId?: string;
-          name?: string | null;
-          ownerId: string;
-          season?: string | null;
-          sport?: string | null;
-          expired?: boolean;
-        };
-        Update: {
-          created_at?: string | null;
-          id?: number;
-          leagueId?: string;
-          name?: string | null;
-          ownerId?: string;
-          season?: string | null;
-          sport?: string | null;
-          expired?: boolean;
-        };
-      };
-      Teams: {
-        Row: {
-          created_at: string | null;
-          espnId: number;
-          id: number;
-          isFavorite: boolean | null;
-          leagueId: string | null;
-          name: string | null;
-          ownerId: string;
-        };
-        Insert: {
-          created_at?: string | null;
-          espnId?: number;
-          id?: number;
-          isFavorite?: boolean | null;
-          leagueId?: string | null;
-          name?: string | null;
-          ownerId: string;
-        };
-        Update: {
-          created_at?: string | null;
-          espnId?: number;
-          id?: number;
-          isFavorite?: boolean | null;
-          leagueId?: string | null;
-          name?: string | null;
-          ownerId?: string;
-        };
-      };
-    };
-    Views: {
-      [_ in never]: never;
-    };
-    Functions: {
-      install_available_extensions_and_test: {
-        Args: Record<PropertyKey, never>;
-        Returns: boolean;
-      };
-    };
-    Enums: {
-      [_ in never]: never;
-    };
-    CompositeTypes: {
-      [_ in never]: never;
+      'fangraphs-constants': Table<FangraphsConstants>;
+      Leagues: Table<League>;
+      Teams: Table<Team>;
     };
   };
 }
