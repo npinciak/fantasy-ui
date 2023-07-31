d1e65d5424e9c1e7e8fddeff2da47470390f8afc
Nathan Pinciak <natepinciak@live.com>
2023-07-31 12:55:58 -0400

    Renamed `fantasy-team-header` to `espn-team-header`

--- src/app/espn/components/espn-team-header/espn-team-header.component.ts
+++ src/app/espn/components/espn-team-header/espn-team-header.component.ts
@@ -2,10 +2,10 @@ import { Component, Input } from '@angular/core';
 import { BaseballTeam } from '@app/espn/mlb/models/baseball-team.model';
 
 @Component({
-  selector: 'app-fantasy-team-header',
-  templateUrl: './fantasy-team-header.component.html',
+  selector: 'app-espn-team-header',
+  templateUrl: './espn-team-header.component.html',
 })
-export class FantasyTeamHeaderComponent {
+export class EspnTeamHeaderComponent {
   @Input() team: BaseballTeam;
   @Input() backgroundColor = '';
 
