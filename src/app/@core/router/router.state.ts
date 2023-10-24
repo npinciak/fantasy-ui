db65aeb685619259f2774d3fa8a6ac557f4d1ab3
Nathan Pinciak <natepinciak@live.com>
2023-10-24 17:27:16 -0400

    Update routing selector and state

--- src/app/@core/router/router.state.ts
+++ src/app/@core/router/router.state.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Action, State, StateContext, Store } from '@ngxs/store';
+import { Action, State, StateContext } from '@ngxs/store';
 import { RouterStateModel } from './router-state.model';
 
 export class SetRouterState {
@@ -15,19 +15,10 @@ export class SetRouterState {
 })
 @Injectable()
 export class RouterState {
-  constructor(private store: Store) {}
+  constructor() {}
 
   @Action(SetRouterState)
-  async setState({ patchState }: StateContext<RouterStateModel>, { payload: { routerStateParams } }: SetRouterState): Promise<void> {
+  setState({ patchState }: StateContext<RouterStateModel>, { payload: { routerStateParams } }: SetRouterState) {
     patchState({ state: routerStateParams });
-    // for (const key in routerStateParams.data) {
-    //   const value = routerStateParams.data[key];
-    //   //   if (IsNonBlockingResolverResult(value)) {
-    //   //     await this.store.dispatch(new SetIsResolverFinished({ name: value.resolverName, isFinished: false })).toPromise();
-    //   //     value.request
-    //   //       .toPromise()
-    //   //       .finally(() => this.store.dispatch(new SetIsResolverFinished({ name: value.resolverName, isFinished: true })));
-    //   //   }
-    // }
   }
 }
