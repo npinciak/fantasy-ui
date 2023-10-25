443f10c786ed8d8e314398e55745f95b9424ceb1
Nathan Pinciak <natepinciak@live.com>
2023-10-25 14:13:02 -0400

    Fix copy and paste error for loading-executor action type

--- src/app/@core/loading-executor/loading-executer.actions.ts
+++ src/app/@core/loading-executor/loading-executer.actions.ts
@@ -1,4 +1,4 @@
 export class SetActionCount {
-  public static readonly type = `[router] SetRouterState`;
+  public static readonly type = `[ngxs_actions_executing] SetActionCount`;
   constructor(public payload: { actionType: string; count: number }) {}
 }
