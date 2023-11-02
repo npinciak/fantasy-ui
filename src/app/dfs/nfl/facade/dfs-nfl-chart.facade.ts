2bc192d043c1560aa4d600af6ca011cba6e5107e
Nathan Pinciak <natepinciak@live.com>
2023-11-02 09:24:40 -0400

    Add new selector file for chart specific features

---
+++ src/app/dfs/nfl/facade/dfs-nfl-chart.facade.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { select } from '@app/@shared/models/typed-select';
+import { DfsNflChartSelector } from '../selectors/dfs-nfl-chart.selector';
+
+@Injectable({ providedIn: 'root' })
+export class DfsNflChartFacade {
+  playerBarChartData$ = select(DfsNflChartSelector.getPlayerBarChartDataByStatAndPosition);
+  playerScatterData$ = select(DfsNflChartSelector.getPlayerScatterData);
+}
