12acf9947fcad075d05af4533762ce966a830480
Nathan Pinciak <natepinciak@live.com>
2023-11-19 17:21:14 -0500

    Fix routing issues

--- src/app/espn-fantasy-baseball/espn-fantasy-baseball-routing.module.ts
+++ src/app/espn-fantasy-baseball/espn-fantasy-baseball-routing.module.ts
@@ -1,8 +1,111 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { UrlPathFragments, UrlPathParams } from '@app/@core/router/url-builder';
+import { BaseballBattersComponent } from './pages/baseball-batters/baseball-batters.component';
+import { BaseballFreeAgentsComponent } from './pages/baseball-free-agents/baseball-free-agents.component';
+import { BaseballHomeComponent } from './pages/baseball-home/baseball-home.component';
+import { BaseballPitchersComponent } from './pages/baseball-pitchers/baseball-pitchers.component';
+import { BaseballPlayerComponent } from './pages/baseball-player/baseball-player.component';
+import { BaseballTeamComponent } from './pages/baseball-team/baseball-team.component';
+import { FantasyBaseballFreeAgentsResolver } from './resolvers/fantasy-baseball-free-agents.resolver';
+import { FantasyBaseballLeagueResolver } from './resolvers/fantasy-baseball-league.resolver';
+import { FantasyBaseballPlayerNewsResolver } from './resolvers/fantasy-baseball-player-news.resolver';
+
+export const routes = [
+  {
+    path: UrlPathFragments.Empty,
+    data: { sport: UrlPathFragments.Baseball },
+    children: [
+      {
+        // year
+        path: UrlPathParams.Year,
+        children: [
+          {
+            path: UrlPathFragments.League,
+            children: [
+              {
+                // league
+                path: UrlPathParams.LeagueId,
+                resolve: [FantasyBaseballLeagueResolver],
+                children: [
+                  {
+                    path: UrlPathFragments.Empty,
+                    component: BaseballHomeComponent,
+                  },
+                  {
+                    path: UrlPathFragments.Team,
+                    children: [
+                      {
+                        // team
+                        path: UrlPathParams.TeamId,
+                        children: [
+                          {
+                            path: UrlPathFragments.Empty,
+                            component: BaseballTeamComponent,
+                          },
+                          {
+                            path: UrlPathFragments.Batters,
+                            children: [
+                              {
+                                path: UrlPathFragments.Empty,
+                                component: BaseballBattersComponent,
+                                data: { position: UrlPathFragments.Batters },
+                              },
+                            ],
+                          },
+                          {
+                            path: UrlPathFragments.Pitchers,
+                            children: [
+                              {
+                                path: UrlPathFragments.Empty,
+                                component: BaseballPitchersComponent,
+                                data: { position: UrlPathFragments.Pitchers },
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                  {
+                    path: UrlPathFragments.FreeAgents,
+                    resolve: [FantasyBaseballFreeAgentsResolver],
+                    children: [
+                      {
+                        path: UrlPathFragments.Empty,
+                        component: BaseballFreeAgentsComponent,
+                      },
+                    ],
+                  },
+                  {
+                    path: UrlPathFragments.Player,
+                    children: [
+                      {
+                        // player
+                        path: UrlPathParams.PlayerId,
+                        resolve: [FantasyBaseballPlayerNewsResolver],
+                        children: [
+                          {
+                            path: UrlPathFragments.Empty,
+                            component: BaseballPlayerComponent,
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
 @NgModule({
-  imports: [RouterModule.forChild([])],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class EspnFantasyBaseballRoutingModule {}
--- src/app/sports-ui/components/leagues-table/leagues-table.component.ts
+++ src/app/sports-ui/components/leagues-table/leagues-table.component.ts
@@ -12,7 +12,7 @@ import {
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ICON_PATH } from '@app/espn/espn.const';
-import { FantasySportToLabelMap, FantasySports, SportLeagueToImageLocationMap } from '@app/espn/models/espn-endpoint-builder.model';
+import { FantasySportToLabelMap, FantasySports, SportToImageLocationMap } from '@app/espn/models/espn-endpoint-builder.model';
 import { SportsUiClientLeague } from '@app/sports-ui/models/sports-ui-league.model';
 import { cellDataAccessor } from '@sports-ui/ui-sdk/helpers';
 
@@ -35,7 +35,7 @@ export class LeaguesTableComponent implements AfterViewInit, OnChanges {
   readonly FantasySports = FantasySports;
   readonly ICON_PATH = ICON_PATH;
   readonly FantasySportToLabelMap = FantasySportToLabelMap;
-  readonly SportLeagueToImageLocationMap = SportLeagueToImageLocationMap;
+  readonly SportLeagueToImageLocationMap = SportToImageLocationMap;
 
   dataSource: MatTableDataSource<SportsUiClientLeague>;
 
