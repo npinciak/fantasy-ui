87384cf86fddfb718aa65e1ff9a919d19e6e1d41
Nathan Pinciak <natepinciak@live.com>
2023-12-08 10:21:15 -0500

    Add live scoring to lineup cards

--- src/app/espn-fantasy-football/selectors/fantasy-football-events.selectors.ts
+++ src/app/espn-fantasy-football/selectors/fantasy-football-events.selectors.ts
@@ -3,16 +3,26 @@ import { GenericSelector } from '@app/@shared/generic-state/generic.selector';
 import { Selector } from '@app/@shared/models/typed-selector';
 import { EspnClient } from '@sports-ui/ui-sdk';
 import { exists } from '@sports-ui/ui-sdk/helpers';
+import { FantasyFootballScoringPeriod } from '../fantasy-football-scoring-period';
 import { FootballPlayer } from '../models/football-player.model';
 import { FantasyFootballEventsState } from '../state/fantasy-football-events.state';
+import { FantasyFootballLeagueSelector } from './fantasy-football-league.selectors';
 import { FantasyFootballTeamSelectors } from './fantasy-football-team.selectors';
 
 export class FantasyFootballEventsSelectors extends GenericSelector(FantasyFootballEventsState) {
-  @Selector([RouterSelector.getTeamId, FantasyFootballTeamSelectors.getTeamStarters, FantasyFootballEventsSelectors.getById])
+  @Selector([
+    RouterSelector.getTeamId,
+    FantasyFootballTeamSelectors.getTeamStarters,
+    FantasyFootballEventsSelectors.getById,
+    FantasyFootballLeagueSelector.slices.seasonId,
+    FantasyFootballLeagueSelector.slices.scoringPeriodId,
+  ])
   static getLiveTeamStats(
     teamId: string | null,
     players: FootballPlayer[],
-    getFootballEventById: (id: string | null) => EspnClient.EventEntity
+    getFootballEventById: (id: string | null) => EspnClient.EventEntity,
+    seasonId: string | null,
+    week: string | null
   ) {
     if (!teamId) throw new Error('teamId cannot be null');
     return players.map(p => {
@@ -20,8 +30,16 @@ export class FantasyFootballEventsSelectors extends GenericSelector(FantasyFootb
 
       const event = eventIds.find(id => getFootballEventById(id.substring(2)));
 
-      const stats = exists(event) ? (exists(p.stats) ? p.stats[event]?.stats : null) : ({} as any);
+      const projectedScoringPeriodId = FantasyFootballScoringPeriod.projectedWeek(seasonId, week);
+
+      const currentScoringPeriodStats = p.stats != null && exists(event) ? p.stats[event] : null;
+      const projectedScoringPeriodStats = p.stats != null ? p.stats[projectedScoringPeriodId] : null;
+
+      const { appliedTotal } = currentScoringPeriodStats != null ? currentScoringPeriodStats : { appliedTotal: null };
+
       const { id, name, injured, injuryStatus, img, team, position, lineupSlotId } = p;
+
+      const totalDelta = delta(appliedTotal, projectedScoringPeriodStats?.appliedTotal ?? null);
 
       return {
         id,
@@ -32,8 +50,17 @@ export class FantasyFootballEventsSelectors extends GenericSelector(FantasyFootb
         team,
         position,
         lineupSlotId,
-        stats,
+        appliedTotal,
+        projectedTotal: projectedScoringPeriodStats?.appliedTotal ?? null,
+        totalDelta,
+        currentScoringPeriodStats,
+        projectedScoringPeriodStats,
       };
     });
   }
+}
+
+function delta(current: number | null, projected: number | null): number {
+  if (current == null || projected == null) return 0;
+  return current - projected;
 }
